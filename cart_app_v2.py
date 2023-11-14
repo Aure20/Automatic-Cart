a2c9966f49209d156d20c53859c23d95e018dbc2
LukasHag <72572341+LukasHag@users.noreply.github.com>
2023-11-14 22:36:37 +0100

    Update cart_app_v2.py
    Improved documentation
    Started with showing next item on Route Page

--- cart_app_v2.py
+++ cart_app_v2.py
@@ -46,14 +46,14 @@ class MyGroceryListApp:
         # suggestions_scrollbar.grid(row=0, column=2, padx=0, sticky='ns')
         # self.suggestions_listbox.config(yscrollcommand=suggestions_scrollbar.set)
 
-        self.suggestions_listbox.bind("<Double-1>", self.add_suggestion_to_list) # double click to add item to shopping list
+        self.suggestions_listbox.bind("<Double-1>", self.add_suggestion_to_list) # double click to add suggested item to shopping list
 
         self.listbox = tk.Listbox(self.list_page, width=20)
         self.listbox.grid(row=1, column=0, columnspan=1, padx=5, pady=10)
 
-        self.listbox.bind("<Double-1>", self.delete_item) # double click to add item to shopping list
+        self.listbox.bind("<Double-1>", self.delete_item) # double click to delete item from shopping list
 
-        send_button = tk.Button(self.list_page, text="Send Shopping List", font=('Comic Sans MS', 12), command=self.get_shopping_list)
+        send_button = tk.Button(self.list_page, text="Send Shopping List", font=('Comic Sans MS', 12), command=self.send_shopping_list) # send shopping list to route maker
         send_button.grid(row=1, column=1)
 
 
@@ -66,20 +66,22 @@ class MyGroceryListApp:
         self.next_item_label = Label(self.route_page, text="")
         self.next_item_label.grid(row=1, column=0, pady=20)
 
-        
-
+        # App startup
         self.home_page.tkraise() # start at home page
 
-        self.root.geometry("300x500")
+        self.root.geometry("300x500") # size of app window
         self.root.title("Supermarket Cart")
-        self.root.resizable(False, False)
+        self.root.resizable(False, False) # make it impossible to resize app
 
         self.create_menu(self.home_page, self.list_page, self.route_page)
-        self.load_items()
+        self.load_items() # load the supermarket items
 
         self.root.mainloop()
 
     def create_menu(self, page1, page2, page3):
+        """
+        Creates a menubar in which you can switch between the different pages.
+        """
         menubar = tk.Menu(self.root)
         self.root.config(menu=menubar)
         file_menu = tk.Menu(menubar, tearoff=0)
@@ -89,69 +91,93 @@ class MyGroceryListApp:
         file_menu.add_command(label="Route", command=lambda: page3.tkraise())
 
     def select_supermarket1(self):
+        """
+        Selects the first supermarket by changing the image directory and updating the supermarket.
+        """
         self.imagedir = self.dir + 'Model1.png'
         self.update_supermarket()
 
     def select_supermarket2(self):
+        """
+        Selects the second supermarket by changing the image directory and updating the supermarket.
+        """
         self.imagedir = self.dir + 'Model2.png'
         self.update_supermarket()
 
     def update_supermarket(self):
-        # Reload the image and recreate the graph based on the selected supermarket
+        """
+        Reload the image and recreate the graph based on the selected supermarket
+        """
         self.image = np.array(Image.open(self.imagedir).convert('RGB'))
-        self.image = self.image[10:210, 10:210, :]
+        self.image = self.image[10:210, 10:210, :] # keep only the supermarket plan inside the frame
         self.graph = create_graph(self.image)
 
-        # Redraw the path based on the new supermarket
-        self.update_shopping_list()
-
-
     def load_items(self):
+        """
+        Load the supermarket items from a json file
+        """
         with open("ImageToGraph/supermarket_items.json", "r") as file:
             data = json.load(file)
             self.supermarket_items = [item for category in data.values() for item in category]
 
     def update_suggestions(self, event):
+        """
+        Give product suggestions based on the user input.
+        """
         user_input = self.item_entry.get()
-        self.suggestions_listbox.delete(0, tk.END)
-        for suggestion in self.supermarket_items:
-            if user_input.lower() in suggestion.lower():
-                self.suggestions_listbox.insert(tk.END, suggestion)
+        self.suggestions_listbox.delete(0, tk.END) # delete suggestions
+        for product in self.supermarket_items: # add supermarket item to the suggestion list if the user input is part of the item
+            if user_input.lower() in product.lower():
+                self.suggestions_listbox.insert(tk.END, product)
 
     def add_suggestion_to_list(self, event):
+        """
+        Add a suggestion to the shopping list.
+        """
         selected_suggestion = self.suggestions_listbox.get(self.suggestions_listbox.curselection())
         if selected_suggestion:
             self.grocery_list.append(selected_suggestion)
             self.update_shopping_list()
 
     def delete_item(self, event):
+        """
+        Remove an existing item from the shopping list.
+        """
         selected_item = self.listbox.get(self.listbox.curselection())[3:] # the first part of the string is a number, a dot and a space
         if selected_item:
             self.grocery_list.remove(selected_item)
             self.update_shopping_list()
 
     def update_shopping_list(self):
+        """
+        Define how an item should be added to the list.
+        """
         self.listbox.delete(0, tk.END)
         for index, item in enumerate(self.grocery_list, start=1):
             self.listbox.insert(tk.END, f"{index}. {item}")
 
-        # Update the next item on the route page
-        
-        if self.grocery_list:
-            next_item = random.choice(self.grocery_list)
-            self.next_item_label.config(text=f"Next Item: {next_item}", font=('Comic Sans MS', 10))
-        else:
-            self.next_item_label.config(text="No items in the shopping list", font=('Comic Sans MS', 10))
+    def send_shopping_list(self):
+        """
+        Base the most optimal path on the shopping list. 
+        Show which item is next on the Route Page
+        """
 
-    def get_shopping_list(self):
-        # with open("grocery_list.json", "w") as file:
-        #     json.dump(self.grocery_list, file)
         print(self.grocery_list)
         items, coordinates, length = hamiltonian_path(self.graph, list(set(self.grocery_list)))
-        print(items)
+        self.grocery_list = items
+        print(self.grocery_list)
 
         paths = [nx.dijkstra_path(self.graph, coordinates[i], coordinates[i+1]) for i in range(len(coordinates)-1)]
         draw_path(self.image, paths)
+
+
+        # Update the next item on the route page
+        
+        if self.grocery_list:
+            next_item = self.grocery_list[0]
+            self.next_item_label.config(text=f"Next Item: {next_item}", font=('Comic Sans MS', 10))
+        else:
+            self.next_item_label.config(text="No items in the shopping list", font=('Comic Sans MS', 10))
 
         return items
 
