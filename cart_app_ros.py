5c24b1749f55b9b59996f4af89dd6aba36c92e89
LukasHag <l.hageman@protonmail.com>
2023-12-17 15:31:56 +0100

    Update cart_app_ros.py
    added a list so that user can see order of items
    
    also other minor updates

--- cart_app_ros.py
+++ cart_app_ros.py
@@ -60,23 +60,25 @@ class MyGroceryListApp:
 
         self.suggest_lb = tk.Label(self.list_page, text="Double click on suggestion to add:", bg=app_background_color, font=('Arial', 8))
         self.suggest_lb.grid(row=0, column=1, pady=10)
-        self.suggestions_listbox = tk.Listbox(self.list_page, width=15)
-        self.suggestions_listbox.grid(row=2, column=1, padx=10, pady=10)
+        self.suggestion_list = tk.Listbox(self.list_page, width=15)
+        self.suggestion_list.grid(row=2, column=1, padx=10, pady=10)
 
-        # suggestions_scrollbar = tk.Scrollbar(self.list_page, orient=tk.VERTICAL, command=self.suggestions_listbox.yview)
+        # suggestions_scrollbar = tk.Scrollbar(self.list_page, orient=tk.VERTICAL, command=self.suggestion_list.yview)
         # suggestions_scrollbar.grid(row=0, column=2, padx=0, sticky='ns')
-        # self.suggestions_listbox.config(yscrollcommand=suggestions_scrollbar.set)
+        # self.suggestion_list.config(yscrollcommand=suggestions_scrollbar.set)
 
-        self.suggestions_listbox.bind("<Double-1>", self.add_suggestion_to_list) # double click to add suggested item to shopping list
+        self.suggestion_list.bind("<Double-1>", self.add_suggestion_to_list) # double click to add suggested item to shopping list
 
-        self.listbox = tk.Listbox(self.list_page, width=15)
-        self.listbox.grid(row=2, column=0, columnspan=1, padx=5, pady=10)
+        self.shopping_listbox = tk.Listbox(self.list_page, width=15)
+        self.shopping_listbox.grid(row=2, column=0, columnspan=1, padx=5, pady=10)
 
-        self.listbox.bind("<Double-1>", self.delete_item) # double click to delete item from shopping list
+        self.shopping_listbox.bind("<Double-1>", self.delete_item) # double click to delete item from shopping list
 
-        send_button = tk.Button(self.list_page, text="Send Shopping List", font=('Kozuka Gothic Pro H', 10), command=self.send_shopping_list, fg="white", bg="#114B5F") # send shopping list to route maker
+        send_button = tk.Button(self.list_page, text="Send Shopping List", font=('Kozuka Gothic Pro H', 8), command=self.send_shopping_list, fg="white", bg="#114B5F") # send shopping list to route maker
         send_button.grid(row=3, column=1, padx=15)
 
+        clear_button = tk.Button(self.list_page, text="Clear List",font=('Kozuka Gothic Pro H', 8), command=self.clear_shopping_list, fg="white", bg="#114B5F") # send shopping list to route maker
+        clear_button.grid(row=3, column=0, padx=15)
 
         # Route page
         self.route_page = Frame(self.root, bg=app_background_color) 
@@ -91,8 +93,8 @@ class MyGroceryListApp:
         self.next_item_label = Label(self.route_page, text="No shopping list known", bg=app_background_color)
         self.next_item_label.grid(row=2, column=0, pady=20)
 
-        picked_button = tk.Button(self.route_page, text="Picked item", font=('Kozuka Gothic Pro H', 12), command=self.picked_item) # user let's know they picked the current item
-        picked_button.grid(row=3, column=0)
+        self.picked_button = tk.Button(self.route_page, text= "Next", font=('Kozuka Gothic Pro H', 12), command=self.picked_item) # user lets know they picked the current item
+        self.picked_button.grid(row=3, column=0)
 
         pause_button =  tk.Button(self.route_page, text="Pause", font=('Kozuka Gothic Pro H', 12), command=self.pause_robot) # pause the shopping cart
         pause_button.grid(row=4, column=0)
@@ -181,16 +183,16 @@ class MyGroceryListApp:
         Give product suggestions based on the user input.
         """
         user_input = self.item_entry.get()
-        self.suggestions_listbox.delete(0, tk.END) # delete suggestions
+        self.suggestion_list.delete(0, tk.END) # delete suggestions
         for product in self.supermarket_items: # add supermarket item to the suggestion list if the user input is part of the item
             if user_input.lower() in product.lower():
-                self.suggestions_listbox.insert(tk.END, product)
+                self.suggestion_list.insert(tk.END, product)
 
     def add_suggestion_to_list(self, event):
         """
         Add a suggestion to the shopping list.
         """
-        selected_suggestion = self.suggestions_listbox.get(self.suggestions_listbox.curselection())
+        selected_suggestion = self.suggestion_list.get(self.suggestion_list.curselection())
         if selected_suggestion:
             self.grocery_list.append(selected_suggestion)
             self.update_shopping_list()
@@ -199,18 +201,26 @@ class MyGroceryListApp:
         """
         Remove an existing item from the shopping list.
         """
-        selected_item = self.listbox.get(self.listbox.curselection())[3:] # the first part of the string is a number, a dot and a space
+        selected_item = self.shopping_listbox.get(self.shopping_listbox.curselection())[3:] # the first part of the string is a number, a dot and a space
         if selected_item:
             self.grocery_list.remove(selected_item)
             self.update_shopping_list()
+        
+    def clear_shopping_list(self):
+        """
+        Clear the whole shopping list and the listbox
+        """
+        self.grocery_list = []
+        self.shopping_listbox.delete(0, tk.END)
+
 
     def update_shopping_list(self):
         """
         Define how an item should be added to the list.
         """
-        self.listbox.delete(0, tk.END)
+        self.shopping_listbox.delete(0, tk.END)
         for index, item in enumerate(self.grocery_list, start=1):
-            self.listbox.insert(tk.END, f"{index}. {item}")
+            self.shopping_listbox.insert(tk.END, f"{item}")
 
     def send_shopping_list(self):
         """
@@ -239,10 +249,19 @@ class MyGroceryListApp:
             self.next_item_label.config(text=f"Next Item: {next_item}", font=('Kozuka Gothic Pro H', 10))
         else:
             self.next_item_label.config(text="No items in the shopping list", font=('Kozuka Gothic Pro H', 10))
+            self.picked_button.config(text="Go to check-out", font=('Kozuka Gothic Pro H', 10))
 
         # Display a confirmation message
-        confirmation_label = Label(self.list_page, text="Shopping list sent!", font=('Kozuka Gothic Pro H', 10), bg="#F3E9D2")
+
+        confirmation_label = Label(self.list_page, text="Shopping list sent! ", font=('Kozuka Gothic Pro H', 10), bg="#F3E9D2")
         confirmation_label.grid(row=3, column=1)
+
+        order = tk.Listbox(self.list_page, width=15)
+        order.grid(row=4, column=1)
+
+        for i in range(len(items)):
+            order.insert(i, f"{i+1}. {items[i]}")
+
 
         # Schedule a function to remove the confirmation message after 3000 milliseconds (3 seconds)
         self.root.after(3000, lambda: confirmation_label.grid_forget())  
@@ -267,8 +286,13 @@ class MyGroceryListApp:
         if self.grocery_list:
             self.grocery_list.pop(0)
             self.paths.pop(0)
+            self.shopping_listbox.delete(0)
+        
+            
         elif self.paths:
             self.paths.pop(0)
+            self.shopping_listbox.delete(0)
+
         print(self.grocery_list)
         print(self.paths)
 
